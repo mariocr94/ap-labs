7d92ce270be86541f79846b3d231e088317856c3
Mario Carrillo <marioacr94@gmail.com>
2020-09-08 19:03:10 +0000

    Solved str-lib

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
+int mystrlen(char *);
+char *mystradd(char *, char *);
+int mystrfind(char *, char*);
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 4){
+        printf("Please use the correct amount of arguments: \n");
+        printf("./main.o <-add|-find> <initial string> <string to add or find>\n");
+        return 0;
+    }
+
+    if(strcmp(argv[1], "-add") == 0){
+        int len = mystrlen(argv[2]);
+        char* newStr = mystradd(argv[2], argv[3]);
+        int newLen = mystrlen(newStr);
+        printf("Initial Length      : %i\n", len);
+        printf("New Str             : %s\n", newStr);
+        printf("New Length          : %i\n", newLen);
+    }else if(strcmp(argv[1], "-find") == 0){
+        int pos = mystrfind(argv[2], argv[3]);
+        printf("['%s'] sring was found at [%i] position\n", argv[3], pos);
+    }else{
+        printf("Please use -add or -find as the second argument\n");
+        return 0;
+    }
+
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,38 @@
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+    int charcount = 0;
+    int i = 0;
+    for(i = 0; str[i]; i++){
+        if(str[i] != '\0')
+            charcount++;
+    }
+    return charcount;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    char *newStr = (char *) malloc(mystrlen(origin) + mystrlen(addition) + 1);
+    strcpy(newStr, origin);
+    strcat(newStr, addition);
+    return newStr;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int len = mystrlen(origin);
+    int sublen = mystrlen(substr);
+    int i, j;
+
+    for(i=0; i<len;i++){
+        if(origin[i] == substr[0]){
+            for (j=1;j<sublen;j++){
+                if (origin[i+j] != substr[j])
+                    break;
+            }
+            if(j == sublen)
+                return i;
+        }
+    }
+    return -1;
 }
