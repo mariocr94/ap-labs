6b80522db221e458cfbf4a4d7692f5eee7f628a5
Mario Carrillo <marioacr94@gmail.com>
2020-11-10 20:16:50 +0000

    Solved goroutines-vs-os-threads
    Signed-off-by: Mario Carrillo <marioacr94@gmail.com>

--- labs/goroutines-vs-os-threads/ping-pong.go
+++ labs/goroutines-vs-os-threads/ping-pong.go
@@ -2,10 +2,44 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
-
+var commsPerSecond int
 func main() {
-	var commsPerSecond int
+	
+	ping := make(chan int)
+	pong := make(chan int)
+	startTime := time.Now()
+	sum := 0
+	go back(ping, pong)
+	go forth(ping, pong)
+	for{
+		time.Sleep(1 * time.Second)
+		fmt.Println("Communications Per Second : ", commsPerSecond)
+		sum += commsPerSecond
+		commsPerSecond = 0
+		if time.Since(startTime)>(10 * time.Second) {
+			sum /= 10
+			fmt.Println("Average communications per second in 10 seconds: ", sum)
+			close(ping)
+			close(pong)
+			return
+		}
+	}
+}
 
-	fmt.Println("Communications Per Second : ", commsPerSecond)
+func back(ping, pong chan int){
+	for {
+		ping<-1
+		commsPerSecond += 1
+		<-pong
+	}
+}
+
+func forth(ping, pong chan int){
+	for {
+		<-ping
+		commsPerSecond += 1
+		pong<-1
+	}
 }
--- labs/goroutines-vs-os-threads/pipeline.go
+++ labs/goroutines-vs-os-threads/pipeline.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
+var token = make(chan string)
 
 func main() {
-	var maxStages, transitTime int
-
+	maxStages := 10000000;
+	first := make(chan time.Time)
 	fmt.Println("Maximum number of pipeline stages   : ", maxStages)
-	fmt.Println("Time to transit trough the pipeline : ", transitTime)
+	go pipeline(first, 0, maxStages)
+	first <- time.Now()
+	<-token	
+	fmt.Println("Maximum number of pipeline stages   : ", maxStages)
+}
+
+func pipeline(first chan time.Time, current int, max int) {
+	if current < max {
+		tiempo := <- first
+		fmt.Println("Routine No.",current, " Time to transit trough the pipeline : ", time.Since(tiempo))
+		go pipeline(first, current+1, max)
+		first<- time.Now()
+	}else{
+		fmt.Println("done")
+		token <- "done"
+	}
 }
